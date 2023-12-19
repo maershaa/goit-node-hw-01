9116dce454038eb2fc7f53b1ebf4c51ca23817fa
maershaa <valeria.work2022@gmail.com>
2023-12-19 20:21:49 +0200

    код с использованием модуля Commander

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
+Используя yargs
 https://ibb.co/XDCR1FZ
 https://ibb.co/pWCwR31
 https://ibb.co/tcP65jj
 https://ibb.co/smWw7pG
+
+
+Используя модуль commander
+https://ibb.co/Fm41y17
+https://ibb.co/r4YY4tX
+https://ibb.co/7jvzWLh
+https://ibb.co/5WnQX5P
--- index.js
+++ index.js
@@ -1,4 +1,57 @@
-const argv = require('yargs').argv;
+// ! Используя модуль commander для парсинга аргументов командной строки. Это более популярная альтернатива модулю yargs
+// Commander - это популярная библиотека для обработки командной строки в Node.js. 
+const { Command } = require('commander');
+const contactService = require("./contacts")
+
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторить
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      const allContacts = await contactService.listContacts();
+      console.table(allContacts);
+      break;
+
+    case 'get':
+      const contactById = await contactService.getContactById(id);
+      console.log(contactById);
+      break;
+
+    case 'add':
+      const newContact = await contactService.addContact({ name, email, phone });
+      console.log(newContact);
+      break;
+
+    case 'remove':
+      const deleteContact = await contactService.removeContact(id);
+      console.log(deleteContact);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
+
+
+
+
+
+//! Используя yargs
+// yargs - это библиотека для обработки аргументов командной строки в Node.js. 
+/* const argv = require('yargs').argv;
 const contactService = require("./contacts")
 
 async function invokeAction({ action, id, name, email, phone }) {
@@ -33,4 +86,4 @@ async function invokeAction({ action, id, name, email, phone }) {
   }
   
   invokeAction(argv);
-  
+   */
